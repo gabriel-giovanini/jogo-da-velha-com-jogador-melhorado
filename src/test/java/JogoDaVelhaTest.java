3d55c702592b9f451f3b907fb979b2474aa7ea99
Gabriel Giovanini <gabrielgiovanini20@gmail.com>
2026-05-01 11:48:26 -0300

    Update JogoDaVelhaTest.java

--- src/test/java/JogoDaVelhaTest.java
+++ src/test/java/JogoDaVelhaTest.java
@@ -1,16 +1,17 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class JogoDaVelhaTest {
+
     @Test
     void verificarJogador() {
-        Jogador jogador = new Jogador('X');
-        assertEquals('X',jogador.getSimbolo());
+        Jogador jogador = new JogadorHumano('X');
+        assertEquals('X', jogador.getSimbolo());
     }
-   
+
     @Test
     void verificarTabuleiro() {
         Tabuleiro tabuleiro = new Tabuleiro();
         assertFalse(tabuleiro.acabouOJogo());
-    }    
+    }
 }
