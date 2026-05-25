94b54211e975e3b9d68caa5e26bd02300d1f4059
Gabriel Giovanini <gabrielgiovanini20@gmail.com>
2026-05-24 22:46:45 -0300

    Alterando JogoDaVelhaTest.java

--- src/test/java/JogoDaVelhaTest.java
+++ src/test/java/JogoDaVelhaTest.java
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
-=======
-import static org.junit.jupiter.api.Assertions.*;
->>>>>>> 3d55c702592b9f451f3b907fb979b2474aa7ea99
 import org.junit.jupiter.api.Test;
 
 public class JogoDaVelhaTest {
@@ -20,8 +16,4 @@ public class JogoDaVelhaTest {
         Tabuleiro tabuleiro = new Tabuleiro();
         assertFalse(tabuleiro.acabouOJogo());
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 3d55c702592b9f451f3b907fb979b2474aa7ea99
